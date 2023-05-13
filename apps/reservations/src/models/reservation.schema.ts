40574d735695c8fc3259554d8ee2855c43da0826
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-13 17:38:19 -0300

    feat: create ReservationDocument and ReservationSchema

--- apps/reservations/src/models/reservation.schema.ts
+++ apps/reservations/src/models/reservation.schema.ts
@@ -1,1 +1,26 @@
-export class Reservation {}
+import { AbstractDocument } from '@app/common/database/abstract.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ versionKey: false })
+export class ReservationDocument extends AbstractDocument {
+  @Prop()
+  timestamp: Date;
+
+  @Prop()
+  startDate: Date;
+
+  @Prop()
+  endDate: Date;
+
+  @Prop()
+  userId: string;
+
+  @Prop()
+  placeId: string;
+
+  @Prop()
+  invoideId: string;
+}
+
+export const ReservationSchema =
+  SchemaFactory.createForClass(ReservationDocument);
