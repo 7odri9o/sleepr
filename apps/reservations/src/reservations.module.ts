ff57edb53deed8684c4c3e0c65afc7f50a166bb8
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-13 16:18:42 -0300

    feat: import DatabaseModule into ReservationsModule

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@app/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
