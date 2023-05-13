cdea0d089b62b493bb2a3315e4edf61683e72855
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-13 17:55:19 -0300

    feat: add ReservationsRepository with dependency imports on ReservationsModule and DatabaseModule

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@app/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
+import { ReservationsRepository } from './reservations.repository';
+import { ReservationDocument } from './models/reservation.schema';
+import { ReservationSchema } from './models/reservation.schema';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ReservationDocument.name,
+        schema: ReservationSchema,
+      },
+    ]),
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
