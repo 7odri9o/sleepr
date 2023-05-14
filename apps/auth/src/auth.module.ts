e5302aba1687b810da3396d032fa76a79fe9db06
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-14 00:30:19 -0300

    refactor: remove custom ConfigModule to setup ConfigModule individually for each app

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,23 +1,32 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from '@app/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { ConfigService } from '@nestjs/config';
-import { ConfigModule } from '@app/common/config/config.module';
 
 @Module({
-  imports: [UsersModule, LoggerModule, JwtModule.registerAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) => ({
-      secret: configService.get('JWT_SECRET'),
-      signOptions: {
-        expiresIn: `${configService.get('JWT_EXPIRATION')}s`
-      }
-    })
-  })],
+  imports: [UsersModule, LoggerModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+      })
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`
+        }
+      })
+    })],
   controllers: [AuthController],
   providers: [AuthService],
 })
