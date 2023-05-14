857bd60e41392f1906f34e8eb32a299385d376ab
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-13 23:42:33 -0300

    feat: use logger and validations on auth app

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3001);
 }
 bootstrap();
