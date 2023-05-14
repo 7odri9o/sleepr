d15b7b4b0ccf44449fc7f307b499d8878c29bf00
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-14 00:14:09 -0300

    feat: register JwtModule

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { LoggerModule } from '@app/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@app/common/config/config.module';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [UsersModule, LoggerModule, JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get('JWT_SECRET'),
+      signOptions: {
+        expiresIn: `${configService.get('JWT_EXPIRATION')}s`
+      }
+    })
+  })],
   controllers: [AuthController],
   providers: [AuthService],
 })
