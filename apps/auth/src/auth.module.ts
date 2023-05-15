526fc67326267fec0da2ac611a9c2d392c4e87dc
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-15 13:17:52 -0300

    feat: local strategy

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,7 @@ import { LoggerModule } from '@app/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [UsersModule, LoggerModule, 
@@ -29,6 +30,6 @@ import { UsersModule } from './users/users.module';
       })
     })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
