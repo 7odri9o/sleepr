2a6e1fe1982b535319ed8b1c14aeb662b8e06293
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-15 23:43:12 -0300

    feat: eslint format on save

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  const configService = app.get(ConfigService)
+  const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
