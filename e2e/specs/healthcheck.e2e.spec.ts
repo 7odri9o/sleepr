530fe8c25958624e5e53b558bffbcd1cdf82fdd7
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2023-05-18 12:46:49 -0300

    feat: add notifications and payments e2e healthcheck tests

--- e2e/specs/healthcheck.e2e.spec.ts
+++ e2e/specs/healthcheck.e2e.spec.ts
@@ -1,3 +1,5 @@
+import { ping } from 'tcp-ping';
+
 describe('Healtcheck', () => {
   test('Auth', async () => {
     const authBaseUrl = process.env.AUTH_BASE_URL;
@@ -9,5 +11,29 @@ describe('Healtcheck', () => {
     const reservationsBaseUrl = process.env.RESERVATIONS_BASE_URL;
     const response = await fetch(reservationsBaseUrl);
     expect(response.ok).toBeTruthy();
+  });
+
+  test('Payments', (done) => {
+    const address = process.env.PAYMENTS_HOST;
+    const port = parseInt(process.env.PAYMENTS_PORT);
+    ping({ address, port }, (error) => {
+      if (error) {
+        fail();
+      }
+
+      done();
+    });
+  });
+
+  test('Notifications', (done) => {
+    const address = process.env.NOTIFICATIONS_HOST;
+    const port = parseInt(process.env.NOTIFICATIONS_PORT);
+    ping({ address, port }, (error) => {
+      if (error) {
+        fail();
+      }
+
+      done();
+    });
   });
 });
